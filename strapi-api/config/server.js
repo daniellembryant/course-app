e3ad4664533f463514153d3accdcdd91fe0d355d
daniellembryant <danbry08@yahoo.com>
2021-11-15 22:00:34 -0600

    Initial backend setup

---
+++ strapi-api/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'd4917aa03625a7474faa608a69100db0'),
+    },
+  },
+});
---
+++ strapi-api/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || '952cfdf7-a586-4829-9dd2-e0d0777cf5a5'
+};
