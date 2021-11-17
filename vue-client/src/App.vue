143604a52a2f7383af2cb7d44915e378203b5ffe
daniellembryant <danbry08@yahoo.com>
2021-11-16 20:53:04 -0600

    adds cards

---
+++ vue-client/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <div id="app">
+    <div v-for="course in courses" v-bind:key="course.id">
+      <div>
+        <b-card
+          :title="course.title"
+          :img-src="`http://localhost:1337${course.image.url}`"
+          :img-alt="course.title"
+          img-top
+          tag="article"
+          style="max-width: 20rem"
+          class="mb-2"
+        >
+          <b-card-text>
+            {{ course.description }}
+          </b-card-text>
+
+          <b-button href="#" variant="primary">Purchase for ${{ course.price }}</b-button>
+        </b-card>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style></style>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      courses: [],
+    };
+  },
+  created() {
+    axios.get("http://localhost:1337/courses").then((response) => {
+      console.log(response.data);
+      this.courses = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
---
+++ vue-client/src/views/Courses.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="courses">
+    <div v-for="course in courses" v-bind:key="course.id">
+      {{ course.title }}
+    </div>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  data: function () {
+    return {
+      courses: [],
+    };
+  },
+  created() {
+    axios.get("http://localhost:1337/courses").then((response) => {
+      console.log(response);
+      this.courses = response.data;
+    });
+  },
+  methods: {},
+};
+</script>
